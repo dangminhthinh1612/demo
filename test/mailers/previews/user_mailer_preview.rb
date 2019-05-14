36a47cd3008f7cb2f630b55a6d0d01ed6fa7e6b6
minhthinh1612 <minhthinh1995@gmail.com>
2019-05-14 23:01:38 +0700

    Add account activation

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,11 @@
+class UserMailerPreview < ActionMailer::Preview
+
+  def account_activation
+    UserMailer.account_activation
+  end
+
+  def password_reset
+    UserMailer.password_reset
+  end
+
+end
---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  test "account_activation" do
+    mail = UserMailer.account_activation
+    assert_equal "Account activation", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "password_reset4" do
+    mail = UserMailer.password_reset4
+    assert_equal "Password reset4", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
